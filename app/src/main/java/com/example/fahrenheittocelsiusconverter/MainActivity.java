f213af6f95b6d4cd4f3ed7ebc3d99fe22e588ff4
majotigartua <majotigartua@gmail.com>
2023-03-16 18:32:44 -0600

    Creation of the FahrenheitToCelsius Android application

---
+++ app/src/main/java/com/example/fahrenheittocelsiusconverter/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.fahrenheittocelsiusconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText fahrenheitTemperatureEditText = findViewById(R.id.fahrenheit_temperature_edittext);
+        TextView celsiusTemperatureTextView = findViewById(R.id.celsius_temperature_textview);
+        Button convertToCelsiusTemperatureButton = findViewById(R.id.convert_to_celsius_temperature_button);
+
+        convertToCelsiusTemperatureButton.setOnClickListener(v-> {
+            String fahrenheitTemperatureInText = fahrenheitTemperatureEditText.getText().toString();
+            if (fahrenheitTemperatureInText.isEmpty()) {
+                Toast.makeText(this, R.string.empty_fields_text, Toast.LENGTH_SHORT).show();
+            } else if (!fahrenheitTemperatureInText.matches("\\d+")) {
+                Toast.makeText(this, R.string.invalid_data_text, Toast.LENGTH_SHORT).show();
+            } else {
+                int fahrenheitTemperature = Integer.parseInt(fahrenheitTemperatureInText);
+                float celsiusTemperature = convertFahrenheitToCelsiusTemperature(fahrenheitTemperature);
+                String celsiusTemperatureInText = String.valueOf(celsiusTemperature);
+                celsiusTemperatureTextView.setText(celsiusTemperatureInText + " °C");
+            }
+        });
+    }
+
+    private float convertFahrenheitToCelsiusTemperature(int fahrenheitTemperature) {
+        float celsiusTemperature = fahrenheitTemperature - 32;
+        celsiusTemperature = (celsiusTemperature*5)/9;
+        return celsiusTemperature;
+    }
+}
